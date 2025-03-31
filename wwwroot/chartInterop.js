005afa04b6ace51f66c83fd1d85066aa8a475cb9
igor <igoral2080@gmail.com>
2025-04-01 01:43:26 +0300

    add support for both datasets, add chart for ILN010 dataset

---
+++ wwwroot/chartInterop.js
@@ -0,0 +1,17 @@
+﻿window.createChart = (canvasId, chartData) => {
+    const ctx = document.getElementById(canvasId).getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: chartData.labels, 
+            datasets: chartData.datasets
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: { title: { display: true, text: 'X-Axis' } },
+                y: { title: { display: true, text: 'Y-Axis' } }
+            }
+        }
+    });
+};
